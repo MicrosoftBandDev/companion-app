50e201c1f8783754c357a2ef0d3ede3f79129eff
Yoshi Askharoun <jjask7@gmail.com>
2021-11-07 20:30:51 -0600

    Added sample code to MainPage for testing on Windows

--- BandCenter/BandCenter.Maui/MainPage.xaml.cs
+++ BandCenter/BandCenter.Maui/MainPage.xaml.cs
@@ -3,6 +3,10 @@ using Microsoft.Band;
 using Microsoft.Maui.Controls;
 using Microsoft.Maui.Essentials;
 
+#if DEBUG
+using System.Diagnostics;
+#endif
+
 namespace BandCenter.Maui
 {
 	public partial class MainPage : ContentPage
@@ -19,6 +23,38 @@ namespace BandCenter.Maui
 #if WINDOWS
 			IBandInfo[] pairedBands = await BandClientManager.Instance.GetBandsAsync();
 			count = pairedBands.Length;
+
+			try
+			{
+                using IBandClient bandClient = await BandClientManager.Instance.ConnectAsync(pairedBands[0]);
+				var version = await bandClient.GetFirmwareVersionAsync();
+
+				// check current user heart rate consent
+				if (bandClient.SensorManager.HeartRate.GetCurrentUserConsent() != UserConsent.Granted)
+				{
+					// user hasn’t consented, request consent
+					await bandClient.SensorManager.HeartRate.RequestUserConsentAsync();
+				}
+
+				// hook up to the Heartrate sensor ReadingChanged event
+				bandClient.SensorManager.HeartRate.ReadingChanged += (sender, args) =>
+				{
+					// do work when the reading changes (i.e., update a UI element)
+					count = args.SensorReading.HeartRate;
+				};
+
+				// start the Heartrate sensor
+				await bandClient.SensorManager.HeartRate.StartReadingsAsync();
+				await System.Threading.Tasks.Task.CompletedTask.WaitAsync(new TimeSpan(0, 0, 10));
+				await bandClient.SensorManager.HeartRate.StopReadingsAsync();
+			}
+			catch (BandException ex)
+			{
+				// Handle a Band connection exception
+#if DEBUG
+				Debug.WriteLine(ex);
+#endif
+			}
 #else
 			count++;
 #endif

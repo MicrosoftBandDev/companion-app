81892eb5aafc8734b4c1b51f45790d6847a7f65f
Yoshi Askharoun <jjask7@gmail.com>
2022-04-05 18:55:37 -0500

    Fixed ActivityTile.Subtitle

--- BandCenter/BandCenter.Maui/Controls/ActivityTile.xaml.cs
+++ BandCenter/BandCenter.Maui/Controls/ActivityTile.xaml.cs
@@ -1,7 +1,5 @@
-using System;
-using Microsoft.Maui;
+using System.Windows.Input;
 using Microsoft.Maui.Controls;
-using Microsoft.Maui.Controls.Xaml;
 
 namespace BandCenter.Maui.Controls
 {
@@ -13,7 +11,7 @@ namespace BandCenter.Maui.Controls
         }
 
         public static readonly BindableProperty SubtitleProperty = BindableProperty.Create(
-            nameof(Subtitle), typeof(string), typeof(ActivityTile), string.Empty);
+            nameof(Subtitle), typeof(string), typeof(ActivityTile), string.Empty, propertyChanged: Subtitle_PropertyChanged);
         public string Subtitle
         {
             get => (string)GetValue(SubtitleProperty);
@@ -26,6 +24,33 @@ namespace BandCenter.Maui.Controls
         {
             get => (string)GetValue(MetricMarkupProperty);
             set => SetValue(MetricMarkupProperty, value);
+        }
+
+        public static readonly BindableProperty CommandParameterProperty = BindableProperty.Create(
+            nameof(CommandParameter), typeof(object), typeof(ActivityTile), null);
+        public object CommandParameter
+        {
+            get => GetValue(CommandParameterProperty);
+            set => SetValue(CommandParameterProperty, value);
+        }
+
+        public static readonly BindableProperty CommandProperty = BindableProperty.Create(
+            nameof(Command), typeof(ICommand), typeof(ActivityTile), null);
+        public ICommand Command
+        {
+            get => (ICommand)GetValue(CommandProperty);
+            set => SetValue(CommandProperty, value);
+        }
+
+        private static void Subtitle_PropertyChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            // For some reason bindings just aren't working properly. ActivityTile gets the prop
+            // changed event, but the Label never gets the new value.
+
+            if (bindable is not ActivityTile tile)
+                return;
+
+            tile.SubtitleLabel.Text = newValue?.ToString();
         }
 
         private static void MetricMarkup_PropertyChanged(BindableObject bindable, object oldValue, object newValue)

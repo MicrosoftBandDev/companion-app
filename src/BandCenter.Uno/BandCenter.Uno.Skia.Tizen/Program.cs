400f442d213632c089f4e4310c551615fbce5ac1
Yoshi Askharoun <jjask7@gmail.com>
2022-04-13 15:00:33 -0500

    Added blank Uno app

---
+++ src/BandCenter.Uno/BandCenter.Uno.Skia.Tizen/Program.cs
@@ -0,0 +1,14 @@
+﻿using Tizen.Applications;
+using Uno.UI.Runtime.Skia;
+
+namespace BandCenter.Uno.Skia.Tizen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = new TizenHost(() => new BandCenter.Uno.App(), args);
+            host.Run();
+        }
+    }
+}
---
+++ src/BandCenter.Uno/BandCenter.Uno.Skia.Wpf/Program.cs
@@ -0,0 +1,3 @@
+﻿namespace BandCenter.Uno.Skia.Gtk
+{
+}

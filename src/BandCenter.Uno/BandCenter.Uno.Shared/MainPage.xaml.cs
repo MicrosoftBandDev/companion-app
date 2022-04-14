1fefde381f644fceaa2f982254cacc6b8d26aafa
Yoshi Askharoun <jjask7@gmail.com>
2022-04-14 18:04:34 -0500

    Fixed Microsoft.Band.Uno reference

--- src/BandCenter.Uno/BandCenter.Uno.Shared/MainPage.xaml.cs
+++ src/BandCenter.Uno/BandCenter.Uno.Shared/MainPage.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Band;
+using Microsoft.Band.Admin.Phone;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -40,7 +42,7 @@ namespace BandCenter.Uno
 
             try
             {
-                using IBandClient bandClient = await BandClientManager.Instance.ConnectAsync(band);
+                using var bandClient = await BandAdminClientManager.Instance.ConnectAsync(band);
                 var version = await bandClient.GetFirmwareVersionAsync();
 
                 // check current user heart rate consent
@@ -66,61 +68,22 @@ namespace BandCenter.Uno
                 await bandClient.SensorManager.HeartRate.StopReadingsAsync();
                 return;
 
-                Guid smsTileId = Guid.Parse("b4edbc35-027b-4d10-a797-1099cd2ad98a");
-                await bandClient.NotificationManager.SendMessageAsync(smsTileId,
-                    "Test Message",
-                    "This isn't a real message",
-                    DateTimeOffset.Now,
-                    Microsoft.Band.Notifications.MessageFlags.ShowDialog);
+                var strip = await bandClient.GetStartStripAsync();
+                var defaultTiles = bandClient.GetDefaultTiles();
+                foreach (var tile in defaultTiles)
+                {
+                    if (strip.Contains(tile.TileId))
+                        continue;
+                    strip.Add(tile);
+                }
+                await bandClient.SetStartStripAsync(strip);
                 return;
 
-                Guid tileId = Guid.Parse("870671A2-E128-4EB6-9F42-D75FA9290B40");
-                Guid tilePageId = Guid.Parse("870671A2-E128-4EB6-9F42-D75FA9290B41");
-                var tilePage = new Microsoft.Band.Tiles.Pages.PageLayout(new Microsoft.Band.Tiles.Pages.FilledPanel(
-                    new Microsoft.Band.Tiles.Pages.TextButton()
-                    {
-                        ElementId = 0x1
-                    }
-                ));
-                var tile = new Microsoft.Band.Tiles.BandTile(tileId)
-                {
-                    Name = "Band Companion",
-                    //SmallIcon = new BitmapImage
-                    //{
-                    //    Stream = async (token) =>
-                    //    {
-                    //        return System.IO.File.OpenRead(@"D:\Pictures\24_testB.png");
-                    //    }
-                    //}.BandIconFromMauiImage(),
-                    //TileIcon = new BitmapImage
-                    //{
-                    //    Stream = async (token) =>
-                    //    {
-                    //        return System.IO.File.OpenRead(@"D:\Pictures\48_testB.png");
-                    //    }
-                    //}.BandIconFromMauiImage(),
-                    PageLayouts =
-                    {
-                        tilePage
-                    }
-                };
-
-                var tiles = await bandClient.TileManager.GetTilesAsync();
-                await bandClient.TileManager.RemoveTileAsync(tileId);
-                bool tileAdded = await bandClient.TileManager.AddTileAsync(tile);
-                if (!tileAdded)
-                {
-                    CounterLabel.Text = "Failed to add tile.";
-                    return;
-                }
-                CounterLabel.Text = "Added tile.";
-
-                await bandClient.TileManager.SetPagesAsync(tileId, new Microsoft.Band.Tiles.Pages.PageData[]
-                {
-                    new Microsoft.Band.Tiles.Pages.PageData(tilePageId, 0, new Microsoft.Band.Tiles.Pages.TextButtonData(0x1, "Howdy from Band Center!"))
-                });
-
-                tiles = await bandClient.TileManager.GetTilesAsync();
+                await bandClient.SendSmsNotificationAsync(10,
+                    "Test Message",
+                    "Howdy from the Admin SDK!",
+                    DateTime.Now);
+                return;
             }
             catch (BandAccessDeniedException ex)
             {
@@ -130,7 +93,7 @@ namespace BandCenter.Uno
             catch (BandException ex)
             {
                 // Handle a Band connection exception
-                Console.WriteLine(ex);
+                Debug.WriteLine(ex);
             }
         }
     }
